e0bc4a4dd338128b8fd19f413f24410cb15ec445
William Sepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-11-10 18:06:52 -0500

    feat :lipstick: se agrega diseño de los demás productos

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -15,11 +15,37 @@ import AsociacionArtesano from '../components/AsociacionArtesano.jsx';
 import Lastenia from '../components/Lastenia.jsx'
 import Retablo from '../components/Retablo.jsx';
 import TallerSanabria from '../components/TallerSanabria.jsx';
+import Bordado from '../components/Bordado.jsx';
+import Ceramica from '../components/Ceramica.jsx';
+import Estampado from '../components/Estampado.jsx';
+import Hojaleteria from '../components/Hojaleteria.jsx';
+import Joyeria from '../components/Joyeria.jsx';
+import Orfeteria from '../components/Orfeteria.jsx';
+import Pintura from '../components/Pintura.jsx';
+import TallaMadera from '../components/TallaMadera.jsx';
+import TallaPiedra from '../components/TallaPiedra.jsx';
+
+
+
+
+
+
+
 const AppRouter = () => (
     <Routes>
         <Route path="/" element={<LoadingPage />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Textileria" element={<Textileria />} />
+        <Route path="/Bordado" element={<Bordado />} />
+        <Route path="/Ceramica" element={<Ceramica />} />
+        <Route path="/Estampado" element={<Estampado />} />
+        <Route path="/Hojaleteria" element={<Hojaleteria />} />
+        <Route path="/Joyeria" element={<Joyeria />} />
+        <Route path="/Orfeteria" element={<Orfeteria />} />
+        <Route path="/Pintura" element={<Pintura />} />
+        <Route path="/TallaMadera" element={<TallaMadera />} />
+        <Route path="/TallaPiedra" element={<TallaPiedra />} />
+
         <Route path="/Perfil" element={<Perfil />} />
         <Route path="/Store" element={<Store />} />
         <Route path="/TallerAwaq" element={<TallerAwaq />} />
