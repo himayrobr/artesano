170bff8c44940e1266bfddf7f628f1c3f2fc0dcd
Guiller022005 <118319910+Guiller022005@users.noreply.github.com>
2024-11-11 12:57:37 -0500

    feat: :sparkles: Ajuste de Approuter, se manejara solo categoria

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -14,17 +14,6 @@ const AppRouter = () => (
     <Routes>
         <Route path="/" element={<LoadingPage />} />
         <Route path="/Home" element={<Home />} />
-        <Route path="/Textileria" element={<Textileria />} />
-        <Route path="/Bordado" element={<Bordado />} />
-        <Route path="/Ceramica" element={<Ceramica />} />
-        <Route path="/Estampado" element={<Estampado />} />
-        <Route path="/Hojaleteria" element={<Hojaleteria />} />
-        <Route path="/Joyeria" element={<Joyeria />} />
-        <Route path="/Orfeteria" element={<Orfeteria />} />
-        <Route path="/Pintura" element={<Pintura />} />
-        <Route path="/TallaMadera" element={<TallaMadera />} />
-        <Route path="/TallaPiedra" element={<TallaPiedra />} />
-
         <Route path="/Perfil" element={<Perfil />} />
         <Route path="/Store" element={<Store />} />
         <Route path="/categoria/:categoriaId" element={<Categoria />} />
