75c7ca9b99257325c36856fb5fd2bf54190806bd
William Sepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-11-12 08:03:35 -0500

    feat :lipstick: se crea diseño de ajustes

--- frontend/src/components/ConfirmacionCompra.jsx
+++ frontend/src/components/ConfirmacionCompra.jsx
@@ -3,22 +3,25 @@ import { Link } from 'react-router-dom';
 import '../styles/ConfirmacionCompra.css';
 
 
+
 import Listo from '../storage/img/Chulo.svg';
 
 function ConfirmacionCompra() {
     return (
-        <div className="confirmacion-compra-container">
-            <div className="icono-compra">
-                <img src={Listo} alt="" />
+        <div className="scrollable-container">
+            <div className="confirmacion-compra-container">
+                <div className="icono-compra">
+                    <img src={Listo} alt="" />
+                </div>
+                <h1>¡Compra realizada con éxito!</h1>
+                <div className="decoracion">◆ ◆ ◆ ◆ ◆ ◆ ◆ ◆ ◆ ◆</div>
+                <p>Gracias por apoyar a los artesanos peruanos, puedes revisar tu compra en la opción de</p>
+                <Link to="/ComprasRealizadas" className="boton-compras">Compras</Link>
+                <div className="decoracion">◆ ◆ ◆ ◆ ◆ ◆ ◆ ◆ ◆ ◆</div>
+                <p>Vincula tu correo para recibir más detalles sobre tus compras realizadas</p>
+                <input type="email" placeholder="Añadir correo electrónico" />
+                <Link to="/home" className="boton-inicio">Regresar al inicio</Link>
             </div>
-            <h1>¡Compra realizada con éxito!</h1>
-            <div className="decoracion"></div>
-            <p>Gracias por apoyar a los artesanos peruanos, puedes revisar tu compra en la opción de</p>
-            <Link to="/ComprasRealizadas" className="boton-compras">compras</Link>
-            <div className="decoracion"></div>
-            <p>Vincula tu correo para recibir más detalles sobre tus compras realizadas</p>
-            <input type="email" placeholder="Añadir correo electrónico" />
-            <Link to="/home" className="boton-inicio">Regresar al inicio</Link>
         </div>
     );
 }
