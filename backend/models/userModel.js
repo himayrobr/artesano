9e777635f598757017cc467126eea6bfc3f71ae4
himayrobr <169061000+himayrobr@users.noreply.github.com>
2024-11-13 07:19:02 -0500

    feat: :sparkles: "estructura arreglada"

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,16 +1,33 @@
+// backend/models/User.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({
-  nombre: String,
-  contraseña: String,
-  email: String,
-  fotoPerfil: String,
-  direccion: String,
-  telefono: String,
-  favoritos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
-  talleresInscritos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Workshop' }],
-  historialCompras: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
-  cupones: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Coupon' }],
-}, { collection: 'User' });
+const userSchemaa = new mongoose.Schema({
+  googleId: { type: String, unique: true, sparse: true },
+  discordId: { type: String, unique: true, sparse: true },
+  facebookId: { type: String, unique: true, sparse: true },
+  email: { type: String, unique: true, sparse: true, default: null },
+  phone: { type: String, unique: true, sparse: true },
+  username: { type: String, required: false, unique: true },  // Asegúrate que el username sea único
+  password: { type: String },
+  photo: { type: String, required: false, default: null },
+  address: { type: String, required: false, default: null },
+  type: { type: String, required: false, default: 'comprador' },
+  favorites: { type: [String], default: [] },
+  workshopsEnrolled: { type: [String], default: [] },
+  acceptedTerms: { type: Boolean, default: true },
+  marketingConsent: { type: Boolean, default: true },
+}, {
+  collection: 'User'
+});
 
-module.exports = mongoose.model('User', userSchema);
+// Encriptar la contraseña antes de guardarla
+userSchemaa.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchemaa);
+module.exports = User;
