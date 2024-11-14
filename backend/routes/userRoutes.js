d30502872dc2a7eb35a5063eab8f2a96dfe9ce43
Ineidy <160775201+Ineidy@users.noreply.github.com>
2024-11-14 13:50:15 -0500

    feat: :sparkles: get favoritos by id

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,12 +5,14 @@ const userController = require('../controllers/userController');
 // router.post('/register', userController.register);
 // router.post('/login', userController.login);
 
-router.put('/:id', userController.updateUser);
+router.get('/:id/favoritos', userController.obtenerFavoritos);
 router.get('/:id', userController.getUserById);
-router.delete('/:id', userController.deleteUserById);
+router.put('/:id', userController.updateUser);
 router.post('/:id/favorites/:productId', userController.addFavorite);
 router.delete('/:id/favorites/:productId', userController.removeFavorite);
 router.post('/:id/workshops/:workshopId', userController.addWorkshop);
 router.delete('/:id/workshops/:workshopId', userController.removeWorkshop);
+router.delete('/:id', userController.deleteUserById);
+
 
 module.exports = router;
